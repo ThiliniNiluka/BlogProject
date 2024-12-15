f6e96fd034e0ca971d8ac972363e552a5815420d
ThiliniNiluka <thilininilukakumari@gamil.com>
2024-12-16 03:43:03 +0530

    create test api

--- api/index.js
+++ api/index.js
@@ -1,22 +1,26 @@
 import mongoose from "mongoose";
-import express from "express"
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js"; 
 
 dotenv.config();
 
+// Connect to MongoDB
 mongoose
   .connect(process.env.MONGO)
   .then(() => {
     console.log("MongoDb is connected");
   })
   .catch((err) => {
-    console.log(err);
+    console.log("Database connection error:", err);
   });
 
 const app = express();
 
+// Middleware to handle routes
+app.use("/api/user", userRoutes);
+
+// Start the server
 app.listen(3000, () => {
   console.log("Server is running on port 3000!!");
 });
-
-
