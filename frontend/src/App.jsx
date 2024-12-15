1850b94e94961ed31456247ce5067d2fe626dddb
ThiliniNiluka <thilininilukakumari@gamil.com>
2024-12-16 01:53:28 +0530

    create the pages & routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import {BrowseRouter as  Router, Route, Routes} from 'react-router-dom'
+import About from './pages/About'
+import Dashboard from './pages/Dashboard'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+import Project from './pages/Project'
+import Home from './pages/Home'
 
-function App() {
-  
-
+export default function App() {
   return (
-  <div>Blog Project</div>
+  <Router>
+    <Routes>
+      <Route path='/' element={<Home />}/>
+      <Route path='/signin' element={<SignIn />}/>
+      <Route path='/signup' element={<SignUp />}/>
+      <Route path='/dashboard' element={<Dashboard />}/>
+      <Route path='/about' element={<About />}/>
+      <Route path='/about' element={<Project />}/>
+    </Routes>
+  </Router>
   )
 }
 
-export default App
+
